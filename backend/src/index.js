830464c0d2f04d8ce9a73d406c376eefcbd9c4d1
Het Virani <hetvirani87@gmail.com>
2026-04-26 23:59:27 +0530

    finish: auth route

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv/config';
 import { connectDB } from './lib/db.js';
+import coookieParser from 'cookie-parser';
 
 const app = express();
 
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(coookieParser());
 
 app.use("/api/auth", authRoutes);
 
