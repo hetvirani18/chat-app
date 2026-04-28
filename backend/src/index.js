eb9bb1d8b6ff8a08b47a1e544019400dbc2aca3b
Het Virani <hetvirani87@gmail.com>
2026-04-28 16:15:33 +0530

    add: message routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv/config';
 import { connectDB } from './lib/db.js';
 import coookieParser from 'cookie-parser';
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(coookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
