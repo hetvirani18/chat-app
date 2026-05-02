83f33a964afcde653830161f60b53a82a22bc73d
hetvirani18 <hetvirani87@gmail.com>
2026-05-02 16:32:51 +0530

    fix: path error

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
 import dotenv from 'dotenv/config';
 import { connectDB } from './lib/db.js';
-import coookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
 
@@ -13,8 +13,7 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json({limit: '10mb'}));
-app.use(express.json());
-app.use(coookieParser());
+app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173',
     credentials: true,
@@ -26,7 +25,7 @@ app.use("/api/messages", messageRoutes);
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/*', (req, res) => {
         res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
     })
 }
