5f06729977442ddc70806d24946006acbd21fd7d
hetvirani18 <hetvirani87@gmail.com>
2026-05-02 16:24:30 +0530

    development ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,10 @@ import coookieParser from 'cookie-parser';
 import cors from 'cors';
 import { app, server } from './lib/socket.js';
 
+import path from 'path';
+
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({limit: '10mb'}));
 app.use(express.json());
@@ -19,6 +22,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    })
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
